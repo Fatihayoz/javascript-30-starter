20ac4558d52d2af4ee39884c2b2f2644682a7c27
Fatih <fatihayoz20800@gmail.com>
2021-06-02 20:47:32 +0200

    index.js file is updated by linking to the listener.js file with import

---
+++ src/listeners/listeners.js
@@ -0,0 +1,12 @@
+/*
+First, we take the event listener.
+Then write a basic function to see if it is working.
+
+*/
+
+
+const hero = document.querySelector('.hero');
+
+// hero.addEventListener('mousemove', (e) => console.log(e));
+
+hero.addEventListener('mousemove', shadow);
