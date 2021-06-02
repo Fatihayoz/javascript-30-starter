03f33a2b6d658a9f2fe91a02819fb91a99087879
Fatih <fatihayoz20800@gmail.com>
2021-06-02 21:52:32 +0200

    commits are added

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -4,12 +4,14 @@ Then write a basic function to see if it is working.
 --> By doing this we will see the coordinates in the console while the mouse moves.
 Second, we write the handler. We have to export handlers.js to make listener to use it.
 --> In the listener part, we have to import what we export in handler.
+Third, we also write the querySelector in the handler
+Forth, we also write variables in the handler
 
 */
 
 import { shadow } from "../handlers/handlers.js";
 
-const hero = document.querySelector('.hero');
+const hero = document.querySelector(".hero");
 
 // hero.addEventListener('mousemove', (e) => console.log(e));
 
