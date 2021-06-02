2bc7e414a761a8620d6d5329238ccc0137cce640
Fatih <fatihayoz20800@gmail.com>
2021-06-02 22:25:04 +0200

    calculateTextShadow function is exported from logic.js to handlers.js

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -5,4 +5,6 @@ const calculateTextShadow (xWalk, yWalk) = {
     ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
     ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
     `;
-};
+}
+
+export { calculateTextShadow };
