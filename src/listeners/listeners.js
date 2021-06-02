2f85f8f2774500736bba824b3830a866e3adf469
Fatih <fatihayoz20800@gmail.com>
2021-06-02 21:17:36 +0200

    import the arrow function from handlers.js to listeners.js

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -2,10 +2,12 @@
 First, we write the event listener.
 Then write a basic function to see if it is working.
 --> By doing this we will see the coordinates in the console while the mouse moves.
-
+Second, we write the handler. We have to export handlers.js to make listener to use it.
+--> In the listener part, we have to import what we export in handler.
 
 */
 
+import { shadow } from "../handlers/handlers.js";
 
 const hero = document.querySelector('.hero');
 
