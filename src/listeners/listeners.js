da4bf4bd6447aa34f8e616e5819c61fcd763f1ee
Fatih <fatihayoz20800@gmail.com>
2021-06-02 21:00:25 +0200

    event listener is added in listeners.js file

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -1,6 +1,8 @@
 /*
-First, we take the event listener.
+First, we write the event listener.
 Then write a basic function to see if it is working.
+--> By doing this we will see the coordinates in the console while the mouse moves.
+
 
 */
 
@@ -9,4 +11,4 @@ const hero = document.querySelector('.hero');
 
 // hero.addEventListener('mousemove', (e) => console.log(e));
 
-hero.addEventListener('mousemove', shadow);
+hero.addEventListener("mousemove", shadow);
