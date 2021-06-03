c3718aa9e8467de2d249d6415e950f842952abea
Fatih <fatihayoz20800@gmail.com>
2021-06-03 12:40:36 +0200

    playsound listener is added

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -3,4 +3,4 @@ import { removeTransition } from "../handlers/handlers.js";
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-window.addEventListener("keydown", playSound);
+window.addEventListener("keydown", playsound);
