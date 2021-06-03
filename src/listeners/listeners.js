9f569abe081f264a40dd73a2e3dfda26f815eae6
Fatih <fatihayoz20800@gmail.com>
2021-06-03 12:38:44 +0200

    init file is updated by importing listeners.js

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -2,3 +2,5 @@ import { removeTransition } from "../handlers/handlers.js";
 
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
