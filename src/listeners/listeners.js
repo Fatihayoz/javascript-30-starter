da5aa0361b0347de9276f35fba7f2ab8b2a830de
Fatih <fatihayoz20800@gmail.com>
2021-06-03 12:36:01 +0200

    removeTransiton is imported in listeners.js file

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -1,2 +1,4 @@
+import { removeTransition } from "../handlers/handlers.js";
+
 const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitionend", (removeTransition)));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
